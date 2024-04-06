53aebb43f8007f4042ede60585d803ebadf75c39
Hrishikesh Nerkar <hrishikesh.nerkar21@pccoepune.org>
2024-04-06 13:02:19 +0530

    HedarerH

---
+++ wt-IE2/src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order your favorite food now!</h2>
+                <p>"Delicious meals delivered to your doorstep. Order now for a delightful dining experience!"</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+
+export default Header
