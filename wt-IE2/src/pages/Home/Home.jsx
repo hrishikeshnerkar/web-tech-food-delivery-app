24806fcc93f3c6a1dd4ba67dd3b42525eec039e8
Hrishikesh Nerkar <hrishikesh.nerkar21@pccoepune.org>
2024-04-06 17:34:39 +0530

    Update Home.jsx

--- wt-IE2/src/pages/Home/Home.jsx
+++ wt-IE2/src/pages/Home/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Home.css'
-import Header from '../../components/Navbar/Header/Header'
+import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 
 
 const Home = () => {
     return (
         <div>
-            <Header/>
-            <ExploreMenu/>
+            <Header />
+            <ExploreMenu />
         </div>
     )
 }
