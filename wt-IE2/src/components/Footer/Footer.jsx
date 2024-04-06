6b608ed1dd53131c39aadc2cca6fe5d071ba1f6c
Darpan Neve <darpanneve3@gmail.com>
2024-04-06 13:05:51 +0530

    router updated

--- wt-IE2/src/components/Footer/Footer.jsx
+++ wt-IE2/src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Footer.css'
-import { assets } from '../../assets'
+import { assets } from '../../assets/assets'
 const Footer = () => {
   return (
     <div className='footer' id='footer'>
       <div className='footer__container'>
         <div className='footer__container__left'>
-          <img src={assets.React} alt='logo' />
+          <img src={assets.app_store} alt='logo' />
           <h2>Footer</h2>
         </div>
         <div className='footer__container__right'>
