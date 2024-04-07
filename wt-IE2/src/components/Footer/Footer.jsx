afdfd7c9114a34be05b9444d1d2251175c2e0f74
Shlok-Nemade07 <shlok.nemade21@pccoepune.org>
2024-04-07 10:40:31 +0530

    footerdone

--- wt-IE2/src/components/Footer/Footer.jsx
+++ wt-IE2/src/components/Footer/Footer.jsx
@@ -4,15 +4,42 @@ import { assets } from '../../assets/assets'
 const Footer = () => {
   return (
     <div className='footer' id='footer'>
-      <div className='footer__container'>
-        <div className='footer__container__left'>
+      <div className='footer-content'>
+        <div className='footer-content-left'>
           <img src={assets.app_store} alt='logo' />
-          <h2>Footer</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus mollitia veritatis molestias nostrum neque enim temporibus ullam? Explicabo, totam in?</p>
+          
+          <div className='footer-social-icons'>
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+
+          </div>
         </div>
-        <div className='footer__container__right'>
-          <h2>Footer</h2>
+
+        <div className='footer-content-center'>
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+
+        </div>
+
+        <div className='footer-content-right'>
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+91 456789321</li>
+            <li>cantact@zomato.com</li>
+          </ul>
         </div>
       </div>
+      <hr />
+      <p className='footer-copyright'>
+        Copyright 
+      </p>
     </div>
   )
 }
